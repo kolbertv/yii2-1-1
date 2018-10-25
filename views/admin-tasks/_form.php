a3bbe4921d1dcfd4a55c46cbf64f68e05f325161
kolbert vladimir <kolbert@yandex.ru>
2018-10-25 17:02:11 +0300

    4 добавлен виджет календарь

--- views/admin-tasks/_form.php
+++ views/admin-tasks/_form.php
@@ -23,11 +23,22 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'created_at')->textInput() ?>
 
-    <?= $form->field($model, 'updated_at')->textInput() ?>
+    <?= $form->field($model, 'updated_at')->widget(\yii\jui\DatePicker::className(), [
+        'dateFormat' => 'yyyy-MM-dd',
+        'language' => 'ru',
+    ]) ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
+
+    <? /* echo \yii\jui\DatePicker::widget([
+            'model' => $model,
+            'attribute' => 'updated_at',
+            'dateFormat' => 'yyyy-MM-dd',
+            'language' => 'ru',
+
+    ]); */ ?>
 
     <?php ActiveForm::end(); ?>
 
